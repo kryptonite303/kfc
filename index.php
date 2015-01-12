aee8edb708151bc3e96a8d22a1cfb461ef00e0a7
Kryptonite <johnchen456@gmail.com>
2015-01-11 23:46:05 -0500

    First commit
    Adding the current files

---
+++ index.php
@@ -0,0 +1,172 @@
+<html>
+	<head>
+	 	<meta charset="utf-8">
+	 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	 	<title>Welcome to Kum Fung Kitchen</title>
+	 	<!-- Bootstrap -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	    <link rel="stylesheet" type="text/css" href="css/custom.css">
+	    <script src="js/respond.js"></script>
+	    <!-- End Bootstrap -->
+	</head>
+	<body>
+		<!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img class="logo1"src="img/koi1.gif" alt="">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+		<!-- <nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">
+		        <img alt="Brand" class="logo1" src="img/koi1.gif">
+		      </a>
+		    </div>
+		  </div>
+		</nav> -->
+		<!-- <div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-header">
+			    <button id="collapser" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+			    	<span class="sr-only">Toggle navigation</span>
+			    	<span class="glyphicon glyphicon-arrow-down"></span>
+			      MENU
+			    </button>
+			</div>
+			<div class="collapse navbar-collapse" id="collapse">
+			    <ul class="nav navbar-nav">
+			    	<a class="navbar-brand" href="#">
+        				<img alt="Brand" class="logo1" src="img/koi1.gif">
+    				</a>
+			        <li><a href="#">Home</a></li>
+			        <li class="dropdown"><a href="#" data-toggle="dropdown">Menu <span class="caret"></span></a>
+			        	<ul class="dropdown-menu">
+			            	<li><a href="#">Main Menu</a></li>
+			                <li><a href="#">Lunch Menu</a></li>
+			            </ul>
+			        </li>
+			        <li class="active"><a href="#">Order</a></li>
+			        <li><a href="#">Contact</a></li>  
+			    </ul> 
+			</div>
+		</div>  -->
+		<!-- end navigation -->
+		<div class="container">
+			<!-- row 1: navigation -->
+	    	
+	    	<div class="row">
+		    	<article class="">
+		            <h1>Welcome</h1>
+		            <p><img src="img/cockatiel.jpg" class="pull-right img-responsive img-rounded">At Kum Fung Kitchen, we serve the finest Chinese food to Take Out or Eat In. We also cater for parties on any occasion.</p>
+		                        <!-- nested row 3a: callouts -->
+		        	<div class="clearfix visible-xs visible-lg"></div>
+		            <div class="row">
+		                <div class="col-lg-3 col-xs-6">
+		                	<div class="thumbnail">
+		                        <div class="caption">
+		                            <h4><span class="glyphicon glyphicon-pushpin"></span> Menu</h4>
+		                            <p>Dogs and cats are susceptible to a variety of illnesses that can be completely prevented by following the appropriate vaccination schedule.</p>
+		                            <p><a href="#" class="btn btn-info" data-toggle="tooltip" title="Read more about vaccines">Read more >></a></p>
+		                        </div>
+		                    </div>
+		                </div>
+		                <div class="col-lg-3 col-xs-6">
+		                     <div class="thumbnail">
+		                        <div class="caption">
+		                            <h4><span class="glyphicon glyphicon-ok"></span> Checkups</h4>
+		                            <p>Regular checkups are a key factor in pet wellness, and can often unearth problems that could lead to health issues down the road.  </p>
+		                            <p><a href="#" class="btn btn-info" data-toggle="tooltip" title="Read more about checkups">Read more >></a></p>
+		                    	</div>
+		                    </div>
+		                </div>
+		                <div class="clearfix visible-md visible-xs"></div>
+		                <div class="col-lg-3 col-xs-6">
+		                	<div class="thumbnail">
+		                        <div class="caption">
+		                            <h4><span class="glyphicon glyphicon-heart"></span> Senior Pets</h4>
+		                            <p>Senior pets generally require more medical attention than their younger counterparts, just as senior humans do. So when is a pet considered “senior”? </p>
+		                            <p><a href="#" class="btn btn-info" data-toggle="tooltip" title="Read more about senior pets">Read more >></a></p>
+		                        </div>
+		                    </div>
+		                </div>
+		                <div class="col-lg-3 col-xs-6">
+		                	<div class="thumbnail">
+		                        <div class="caption">
+		                    		<h4><span class="glyphicon glyphicon-cutlery"></span> Diet Plans</h4>
+		                    		<p>Wisdom veterinarians have all had extensive training in pet nutrition and are your best resources when considering changing your pet’s diet. </p>
+		                     		<p><a href="#" class="btn btn-info" data-toggle="tooltip" title="Read more about diet plans">Read more >></a></p>
+		                    	</div>
+		                    </div>
+		                 </div>
+		            </div><!-- end nested row 3a -->
+		        </article>
+	        </div><!-- end row 3 -->
+    	</div> <!-- end container -->
+        <!--
+        <aside class="col-lg-3 col-sm-4 col-lg-pull-9 col-sm-pull-8">
+        	<h3>About Our Services</h3>
+            <ul class="nav nav-pills nav-stacked">
+                <li class="active"><a href="#">Vaccinations</a></li>
+                <li><a href="#">Checkups</a></li>
+                <li><a href="#">Senior Pets</a></li>
+                <li><a href="#">Diet Plans</a></li>
+            </ul> 
+         </aside>
+    	-->
+    	<!-- row 2: header -->
+    <!--
+    <header class="row">
+    	<div class="col-lg-6 col-sm-5">
+        	<a href="index.html"><img src="img/logo.png" alt="Wisdom Pets. click for home." class="img-responsive"></a>
+        </div>
+    	<div class="col-lg-6 col-sm-7">
+        	<img src="img/animals.jpg" alt="" class="hidden-xs img-responsive">
+        </div>
+    </header>
+    -->
+	    <!-- row 4
+	    <footer class="row">
+	         <p><small>This not a real veterinary medicine site, and is not meant to diagnose or offer treatment. Please see your veterinarian for all matters related to your pet's health.</small></p>
+	         <p><small>Wisdom Pet Medicine is a training brand owned by lynda.com.</small></p>
+	    </footer>
+	    -->
+	<!--
+		            <ol class="breadcrumb">
+		              <li><a href="#">Home</a> <span class="glyphicon glyphicon-circle-arrow-right"></span></li>
+		              <li><a href="#">Services</a> <span class="glyphicon glyphicon-circle-arrow-right"></span></li>
+		              <li class="active">Vaccinations</li>
+		            </ol>
+		        	-->
+	<!-- javascript -->
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
